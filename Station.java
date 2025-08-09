6fa8058c2d4eec6bacf04af10a0182054215cfe1
Michael Avrahami <michaela638@nycstudents.net>
2025-08-09 14:48:45 -0400

    Wrote getter

---
+++ Station.java
@@ -0,0 +1,22 @@
+public class Station {
+    private String name;
+    private String borough;
+    private String lines;
+    private double latitude;
+    private double longitude;
+
+    public Station(String name, String borough, String lines, double lat, double long){
+        this.name = name;
+        this.borough = borough;
+        this.lines = lines;
+        this.latitude = lat;
+        this.longitude = long;
+    }
+
+    public getName
+
+    public dist(Station other){
+
+    }
+
+}
