2a22703d0bc5c2dade4e6c77a1a60f21da5f61df
Julien Diamond <juliendiamond@MacBook-Pro.local>
2025-08-09 15:18:26 -0400

    created main

---
+++ main.java
@@ -0,0 +1,9 @@
+public class main {
+
+  static String numberOfNumbersName = "MTA_Subway_Stations_20250809.csv";
+
+  public static void main(String[] args) {
+
+    FileHandler.getStationList(numberOfNumbersName);
+  }
+}
